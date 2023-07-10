4eafb0f175bef6f9998be268990359f5cb3d0573
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-07-09 19:05:45 -0500

    Start checkpoint viewer and check list

---
+++ PDF Acc ToolSet/Tools/Checker/ConditionViewer.cs
@@ -0,0 +1,43 @@
+﻿using PDF_Acc_ToolSet.Utils;
+using PDF_Acc_ToolSet.Utils.Checks;
+
+namespace PDF_Acc_ToolSet.Tools.Checker
+{
+    /// <summary>
+    /// A better name would be checkpoint viewer :)
+    /// </summary>
+    public partial class ConditionViewer : Form
+    {
+        public ConditionViewer(Checkpoint checkpoint)
+        {
+            InitializeComponent();
+            // For each check in the checkpoint, add it to the flow panel
+            foreach (Check check in checkpoint.checks)
+            {
+                SingleCondition singleCondition = new(check, "None");
+                singleCondition.PassedCheck += (sender, e) =>
+                {
+                    // The check was passed manually
+                    check.status = CheckStatus.Success;
+                    UpdateCheckpointStatus();
+                };
+                CheckFlow.Controls.Add(singleCondition);
+            }
+            foreach (Check check in checkpoint.checks)
+            {
+                SingleCondition singleCondition = new(check, "None");
+                CheckFlow.Controls.Add(singleCondition);
+            }
+            foreach (Check check in checkpoint.checks)
+            {
+                SingleCondition singleCondition = new(check, "None");
+                CheckFlow.Controls.Add(singleCondition);
+            }
+        }
+
+        private void UpdateCheckpointStatus()
+        {
+            // Check if all have been passed/failed
+        }
+    }
+}
---
+++ PDF Acc ToolSet/Tools/Checker/SingleCondition.cs
@@ -0,0 +1,52 @@
+﻿using PDF_Acc_ToolSet.Utils;
+
+namespace PDF_Acc_ToolSet.Tools.Checker
+{
+    public partial class SingleCondition : UserControl
+    {
+        public event EventHandler<Check> PassedCheck;
+        private Check check;
+
+        private SingleCondition()
+        {
+            InitializeComponent();
+        }
+
+        public SingleCondition(Check check, string hint)
+        {
+            InitializeComponent();
+            this.check = check;
+
+            // Set the name
+            NameLbl.Text = check.index;
+            // Set the hint (if any)
+            if (hint != null)
+            {
+                HintLbl.Text = hint;
+            }
+            // Set the status
+            if (check.status == CheckStatus.Failure)
+            {
+                StatusLbl.Text = "❌";
+                StatusLbl.ForeColor = Color.Red;
+            }
+            else
+            {
+                StatusLbl.Text = "✓";
+                StatusLbl.ForeColor = Color.ForestGreen;
+            }
+
+            // If a manual check, enable the button for passing it
+            if (check.validator == Validator.Human)
+            {
+                PassBtn.Enabled = true;
+            }
+        }
+
+        private void PassBtn_Click(object sender, EventArgs e)
+        {
+            // Emit that the check should be passed
+            PassedCheck?.Invoke(this, check);
+        }
+    }
+}

ca127551fd8e4a656e1a0e6689c3099bf6af7eec
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-09-16 11:07:40 -0500

    Remove android, ios, tizen support. Remove win file picker

--- Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace PDF_Acc_Toolset;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
